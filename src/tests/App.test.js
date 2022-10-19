ae1dd4aa67f6ef0aa494e48c664541e0af9c0388
Joana <joanamdsantos1@gmail.com>
2022-10-19 15:51:56 -0300

    [200~1. Teste o componente <App.js />

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,54 @@
-test('', () => {});
+import { screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa as rotas da aplicação Pokedex', () => {
+  it('Testa o link Home', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkHome = screen.getByText('Home');
+    expect(linkHome).toBeInTheDocument();
+
+    userEvent.click(linkHome);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Testa o link About', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkAbout = screen.getByText('About');
+    expect(linkAbout).toBeInTheDocument();
+
+    userEvent.click(linkAbout);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Testa o link Favorite Pokémons', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkFavorites = screen.getByText('Favorite Pokémons');
+    expect(linkFavorites).toBeInTheDocument();
+
+    userEvent.click(linkFavorites);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Testa se é redirecionado a página Not Found ao entrar em url desconhecida', () => {
+    const { history, getByRole } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/trybe');
+    });
+
+    const { pathname } = history.location;
+
+    const textNotFound = getByRole('heading', { level: 2, name: /page requested/i });
+    expect(textNotFound).toBeInTheDocument();
+    expect(pathname).toBe('/trybe');
+  });
+});
---
+++ src/tests/renderWithRouter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+
+  const allSelectors = render(
+    <Router history={ history }>
+      { component }
+    </Router>,
+  );
+
+  return { ...allSelectors, history };
+};
+
+export default renderWithRouter;
