d33a29c827b9d1961dc887878b667c5999b1ec3f
Joana <joanamdsantos1@gmail.com>
2022-10-19 16:53:48 -0300

    3. Teste o componente <FavoritePokemons.js />

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Testa o componente FavoritePokemons', () => {
+  it('Testa se é exibida a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const noFavoriteFound = screen.getByText(/no favorite/i);
+    expect(noFavoriteFound).toBeInTheDocument();
+  });
+});
