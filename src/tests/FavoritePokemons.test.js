0267ba4ceac69492bb60cd2ff5c80daab93c0c24
Joana <joanamdsantos1@gmail.com>
2022-10-20 10:23:25 -0300

    Adicionando o teste para checar lista de favoritos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,6 +1,8 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import FavoritePokemons from '../pages/FavoritePokemons';
+import App from '../App';
 
 describe('Testa o componente FavoritePokemons', () => {
   it('Testa se é exibida a mensagem No favorite pokemon found', () => {
@@ -8,5 +10,19 @@ describe('Testa o componente FavoritePokemons', () => {
 
     const noFavoriteFound = screen.getByText(/no favorite/i);
     expect(noFavoriteFound).toBeInTheDocument();
+  });
+
+  it('Testa se são exibidos os cards de pokémons favoritados', () => {
+    renderWithRouter(<App />);
+
+    const detailsLink = screen.getByRole('link', { name: /details/i });
+    userEvent.click(detailsLink);
+
+    const favoriteCheck = screen.getByLabelText(/favoritado/i);
+    userEvent.click(favoriteCheck);
+
+    renderWithRouter(<FavoritePokemons />);
+    const pokemon = screen.getByTestId('pokemon-name');
+    expect(pokemon).toBeInTheDocument();
   });
 });
